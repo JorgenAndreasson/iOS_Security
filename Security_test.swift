23a0a52653dd4a31a0f959ddd1a7d1fbc696cfa2
Jorgen Andreasson <iOS@clockwork.ie>
2020-03-02 09:46:31 +0000

    Commented out Answer logging from Fabric.

--- Security_test.swift
+++ Security_test.swift
@@ -1,10 +1,10 @@
-/ MARK:- Security test
+// MARK:- Security test
 extension AppDelegate {
     
     /// Test to see if the app has been compromised
     private func testInfoPlist() -> Void {
         if Bundle.main.object(forInfoDictionaryKey: "SignerIdentity") != nil {
-            Answers.logCustomEvent(withName: "Security Breach", customAttributes: ["Status": "Compromised app"])
+           // Answers.logCustomEvent(withName: "Security Breach", customAttributes: ["Status": "Compromised app"])
             fatalError("Compromised app")
         }
     }
@@ -21,14 +21,14 @@ extension AppDelegate {
                 || FileManager.default.fileExists(atPath: "/private/var/lib/apt/")
                 || UIApplication.shared.canOpenURL(URL(string: "cydia://package/com.example.package")!) {
                 
-                Answers.logCustomEvent(withName: "Security Breach", customAttributes: ["Status": "Jailbroken device"])
+              //  Answers.logCustomEvent(withName: "Security Breach", customAttributes: ["Status": "Jailbroken device"])
                 fatalError("Jailbroken device")
             }
             // Check 2 : Reading and writing in system directories (sandbox violation)
             let testString = "TestString"
             do {
                 try testString.write(toFile: "/private/testString.txt", atomically: true, encoding: String.Encoding.utf8)
-                Answers.logCustomEvent(withName: "Security Breach", customAttributes: ["Status": "Jailbroken device"])
+              //  Answers.logCustomEvent(withName: "Security Breach", customAttributes: ["Status": "Jailbroken device"])
                 fatalError("Jailbroken device")
             } catch {
                 return
